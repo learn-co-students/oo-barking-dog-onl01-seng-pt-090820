b8b289302a2655328e2be47fe119c7e8b9c27a77
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-21 19:13:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 # Your code goes here!
-def dog(dog)
+class Dog 
+  
+  def name=(dog_name)
+    @this_dogs_name = (dog_name)
+  end
+  
+  def name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+  
 end
